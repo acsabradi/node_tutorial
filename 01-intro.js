6336ea361825cb4af0b3cac96fb8d2eeb7a30096
Attila Csábrádi <attila.csabradi@outlook.com>
2021-06-16 00:16:27 +0200

    globals and interval

---
+++ 01-intro.js
@@ -0,0 +1,13 @@
+ // Globals
+
+ // __dirname   - path to current directory
+ // __filename  - file name
+ // require     - function to use modules (CommonJS)
+ // module      - info about current module/file
+ // process     - info about executor environment
+
+ console.log(__dirname);
+
+ setInterval(() => {
+     console.log("Hello world")
+ }, 1000);

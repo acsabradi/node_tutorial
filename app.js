c0df87a20cc292070b0c60477fe85117b1a0d4df
Attila Csábrádi <attila.csabradi@outlook.com>
2021-06-16 23:37:01 +0200

    alternative flavor for modules

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const names = require('./04-names')
 const sayHi = require('./05-utils')
+const data = require('./06-alternative-module-flavor')
 
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
+
+console.log(data);

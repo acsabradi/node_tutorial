06a56cc6185d3c98947417e1454d3993f3711614
Attila Csábrádi <attila.csabradi@outlook.com>
2021-06-30 23:13:48 +0200

    os module lesson

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const names = require('./04-names')
-const sayHi = require('./05-utils')
-const data = require('./06-alternative-module-flavor')
-const mindGrenade = require('./07-mind-grenade')
+const os = require('os')
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+// info about the current user
+const user = os.userInfo()
+console.log(user)
 
-console.log(data);
+// returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds.`)
+
+// info about the running system
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+console.log(currentOS);
